e72fddb62c679cfa2beda90ace2813c19d0870f3
Yurine-Sama <Yurine2019@gmail.com>
2022-02-25 00:02:21 +0700

    c7

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+        public void getAnimatorHipHop(){
+            this.GetComponent<Animator>().Play("Dance HipHop");
+        }
+
+     public void getAnimatorTwerk(){
+        this.GetComponent<Animator>().Play("Dance Twerk");
+    }
+
+       public void getAnimatorSneak(){
+        this.GetComponent<Animator>().Play("Sneak Walk");
+    }
+}
---
+++ Assets/ChangeColorForSphere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorForSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getyellow(){
+        this.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void getBlue(){
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void getBlack(){
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
